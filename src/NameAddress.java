90976259f0542997c31ad5e53cec1a23601831b0
localbuddha <localbuddha@gmail.com>
2024-02-22 19:42:36 +0200

    NameAddress.java was added.

--- src/NameAddress.java
+++ src/NameAddress.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+/*
+Define two String variables name and address. Output question "What is your name?
+to the console. Read the value of name and output next question: “Where do you live,
+(name)?“ to the console. Read the value of address from the console and output the
+complete information.
+ */
 public class NameAddress {
 
 
