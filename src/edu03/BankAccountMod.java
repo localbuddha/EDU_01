908f605c23f9da997fb9b53848f5b1795cc291e8
localbuddha <localbuddha@gmail.com>
2024-03-11 12:58:01 +0200

    Added HW01, HW02, HW03 into EDU04.

--- src/edu03/BankAccountMod.java
+++ src/edu03/BankAccountMod.java
@@ -1,22 +1,41 @@
 package edu03;
 
 public class BankAccountMod {
-    //fields
+    public static void main(String[] args) {
+
+    }
+
+    private String accountNumber;
+    private double balance;
+    public int depo = 256;
+
     public BankAccountMod(String accountNumber, double initialBalance) {
-        //  this.accountNumber = accountNumber;
-        // this.balance = initialBalance;
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
     }
 
-    public void deposit(String accountNumber, double initialBalance) {
-        this.getBalance();
-
+    public void deposit(double balance, int depo) {
+        this.balance += depo;
     }
 
-    public void withdraw() {//add paremeter in method
-        //TODO
+    public void withdraw(String accountNumber, int withdrawAmount) {
+        if (withdrawAmount < balance) {
+            balance = balance - withdrawAmount;
+        }
+    }
+
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
     }
 
     public double getBalance() {
-        return 0;//TODO
+        return balance;
     }
+
+
 }
